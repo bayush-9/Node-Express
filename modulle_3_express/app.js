c144c4fb3dc07a00e617c83027f90c445872cd60
Ayush Anil Bharsakle <bharsakleaa@gmail.com>
2021-11-15 09:36:06 +0530

    Shop app html pages and css added

--- modulle_3_express/app.js
+++ modulle_3_express/app.js
@@ -1,17 +1,22 @@
+const path = require('path'); 
+
 const express = require('express');
 
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const customerRoutes = require('./routes/shop')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin',adminRoutes);
+app.use(customerRoutes);
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next)=>{
+  res.status(404);
+  res.sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
